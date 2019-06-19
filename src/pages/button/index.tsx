dba893ff1ec45af5afd22c8ca3cc34931ee6ed6e
李斌 <bin.li@quantgroup.cn>
2019-06-19 11:39:41 +0800

    Feat： button update

--- src/pages/button/index.tsx
+++ src/pages/button/index.tsx
@@ -7,28 +7,51 @@ export default class Content extends React.Component {
   render() {
     const btnAry: BtnChildProps[] = [
       {
+        content: "页面主操作",
         size: "xl",
         type: "primary"
       },
       {
+        content: "页面主操作",
         size: "lg",
         type: "primary",
         disabled: true
       },
       {
+        content: "页面次要操作",
         size: "lg"
       },
       {
+        content: "页面次要操作",
         size: "lg",
         disabled: true
       },
       {
+        content: "警告操作",
         size: "lg",
         type: "warning"
       },
       {
+        content: "警告操作",
         size: "lg",
         type: "warning",
+        disabled: true
+      }
+    ];
+    const inlineBtnAry: BtnChildProps[] = [
+      {
+        content: "按钮",
+        type: "primary"
+      },
+      {
+        content: "按钮"
+      },
+      {
+        content: "警告",
+        type: "warning"
+      },
+      {
+        content: "禁止",
         disabled: true
       }
     ];
@@ -40,6 +63,7 @@ export default class Content extends React.Component {
           <WhiteSpace />
           <div className="title-ch">按钮</div>
           {createButtons(btnAry)}
+          {createInlineButtons(inlineBtnAry)}
         </WingBlank>
       </div>
     );
@@ -47,6 +71,7 @@ export default class Content extends React.Component {
 }
 
 interface BtnChildProps {
+  content: string;
   size?: "sm" | "md" | "lg" | "xl" | "xs";
   type?: "primary" | "warning" | "ghost";
   disabled?: boolean;
@@ -58,9 +83,26 @@ function createButtons(btnAry: BtnChildProps[]) {
       <React.Fragment>
         <WhiteSpace size={item.size} />
         <Button type={item.type} disabled={item.disabled}>
-          Primary
+          {item.content}
         </Button>
       </React.Fragment>
     );
   });
+}
+function createInlineButtons(btnAry: BtnChildProps[]) {
+  return (
+    <React.Fragment>
+      <WhiteSpace size="lg" />{" "}
+      {btnAry.map(item => {
+        return (
+          <React.Fragment>
+            <Button type={item.type} inline={true} disabled={item.disabled}>
+              {item.content}
+            </Button>
+            <span className="button-right-space" />
+          </React.Fragment>
+        );
+      })}
+    </React.Fragment>
+  );
 }
