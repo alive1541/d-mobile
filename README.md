83b091d88b33ae8b0005eb4352947dfd62c55c04
李斌 <bin.li@quantgroup.cn>
2019-07-17 23:02:33 +0800

    feat: patch

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 预览地址：
 http://39.100.100.217
+
+启动项目：
+npm install
+npm start
--- src/mc/dialog/lazyRenderBox.tsx
+++ src/mc/dialog/lazyRenderBox.tsx
@@ -7,6 +7,9 @@ export interface lazyRenderProps {
 }
 
 export default class LazyRender extends React.Component<lazyRenderProps, any> {
+  shouldComponentUpdate(nextProps: lazyRenderProps) {
+    return !!nextProps.visible;
+  }
   render() {
     const props: any = { ...this.props };
     delete props.visible;
